108c8c41315bbfb19570d924838d865d08773486
guvon1982 <guvon1982@gmail.com>
2025-11-03 15:43:16 -0300

    Fase 1: Configura estrutura de pastas e servidor Express inicial

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    message: 'Olá, Mundo! Esta é a API do Gerenciador de Tarefas.'
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
